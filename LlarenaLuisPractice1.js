1c60a8885eab8f45055a5ccf35ddccf2afea9c54
luisvicente26 <luisvicente.llarena.iics@ust.edu.ph>
2020-02-16 22:48:18 +0800

    first commit

---
+++ LlarenaLuisPractice1.js
@@ -0,0 +1,22 @@
+function check2(chkOther) {
+    var dvOther = document.getElementById("dvOther");
+    dvOther.style.display = chkOther.checked ? "block" : "none";
+}
+
+
+function check4(chkPC) {
+    var dvPC = document.getElementById("dvPC");
+    dvPC.style.display = chkPC.checked ? "block" : "none";
+}
+
+function check5(chkHCP) {
+    var dvHCP = document.getElementById("dvHCP");
+    dvHCP.style.display = chkHCP.checked ? "block" : "none";
+}
+
+function Submit(){
+    alert("Thank you for submitting!");
+}
+
+
+
